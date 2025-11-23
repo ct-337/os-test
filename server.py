c1136b5f5c41b6dbc609de0a596c7bd6f96b2c80
ct-337 <themathematicalcomet@gmail.com>
2025-11-23 00:27:42 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import os
 
 app = Flask(__name__)
 
+def get_browser():
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
+
 @app.route('/')
 def home():
     return "Browser service running"
+
+@app.route('/browse/<path:url>')
+def browse(url):
+    driver = get_browser()
+    driver.get(f'https://{url}')
+    title = driver.title
+    html = driver.page_source
+    driver.quit()
+    return jsonify({'title': title, 'html': html[:500]})
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
